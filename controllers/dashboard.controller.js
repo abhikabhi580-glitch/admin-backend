531b1924443c8c314c25c1341f7035f95a0516a3
abhidobariya <abhidobariya2212@gmail.com>
2025-07-28 19:11:05 +0530

    changes for dashboard

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,17 +1,15 @@
-const Character = require('../models/character.model');
-const Pet = require('../models/pet.model');
-const Vehicle = require('../models/vehicle.model');
+const db = require('../config/db'); // your MySQL connection (using mysql2)
 
 exports.getDashboardSummary = async (req, res) => {
     try {
-        const totalCharacters = await Character.countDocuments();
-        const totalPets = await Pet.countDocuments();
-        const totalVehicles = await Vehicle.countDocuments();
+        const [characterRows] = await db.promise().query("SELECT COUNT(*) AS totalCharacters FROM characters");
+        const [petRows] = await db.promise().query("SELECT COUNT(*) AS totalPets FROM pets");
+        const [vehicleRows] = await db.promise().query("SELECT COUNT(*) AS totalVehicles FROM vehicles");
 
         res.json({
-            totalCharacters,
-            totalPets,
-            totalVehicles,
+            totalCharacters: characterRows[0].totalCharacters,
+            totalPets: petRows[0].totalPets,
+            totalVehicles: vehicleRows[0].totalVehicles,
         });
     } catch (err) {
         res.status(500).json({ error: err.message });
