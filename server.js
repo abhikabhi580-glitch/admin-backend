58bc2abf6047c180fdb69be1e8996974b72d68cf
abhidobariya <abhidobariya2212@gmail.com>
2025-07-28 19:16:12 +0530

    changes

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
-const db = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-// MySQL connection check
-db.connect((err) => {
-    if (err) {
-        console.error('❌ MySQL connection error:', err.message);
-        process.exit(1);
-    }
-
-    console.log('✅ Connected to MySQL');
-
-    app.listen(PORT, () => {
-        console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
